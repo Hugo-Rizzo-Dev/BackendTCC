8882ed6d9db374f67034cabe1fdd2bbf01ec0031
Hugo-Rizzo-Dev <rizzo_hugo@hotmail.com>
2025-10-06 13:02:32 -0300

    Update code

--- server.js
+++ server.js
@@ -1185,7 +1185,7 @@ app.post("/validate-report", async (req, res) => {
         `;
 
     const apiKey = process.env.GEMINI_API_KEY;
-    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro-vision:generateContent?key=${apiKey}`;
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`;
 
     const payload = {
       contents: [
